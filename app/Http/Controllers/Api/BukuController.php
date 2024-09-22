f1e61bdda97ed23a8c12119783d3bb083684be6f
Arjuna Satria <arjunasatria1717@gmail.com>
2024-09-22 22:39:54 +0700

    [FEATURES] Add Buku Controller

---
+++ app/Http/Controllers/Api/BukuController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Buku;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BukuController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'judul' => 'required|string|max:255',
+            'pengarang' => 'required|string|max:255',
+            'penerbit' => 'required|string|max:255',
+            'tahun_terbit' => 'required|integer|min:1900|max:' . date('Y'),
+            'status' => 'required|in:tersedia,dipinjam,hilang',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $buku = Buku::create([
+            'judul' => $request->judul,
+            'pengarang' => $request->pengarang,
+            'penerbit' => $request->penerbit,
+            'tahun_terbit' => $request->tahun_terbit,
+            'status' => $request->status,
+        ]);
+
+        return response()->json([
+            'message' => 'Buku berhasil ditambahkan',
+            'data' => $buku,
+        ], 201);
+    }
+
+    public function getAll(Request $request)
+    {
+        if (!$request->user()) {
+            return response()->json(['message' => 'Anda perlu login'], 401);
+        }
+
+        $buku = Buku::all();
+        return response()->json([
+            'message' => 'Data Buku',
+            'data' => $buku,
+        ], 200);
+    }
+
+    public function searchByJudul(Request $request)
+    {
+        $request->validate([
+            'judul' => 'required|string|min:2|max:100',
+        ]);
+
+        $judul = $request->input('judul');
+
+        $prefix = substr($judul, 0, 2);
+
+        $buku = Buku::where('judul', 'like', $prefix . '%')->get(['id', 'judul', 'pengarang', 'penerbit', 'tahun_terbit', 'status']);
+
+        if ($buku->isEmpty()) {
+            return response()->json(['message' => 'Buku belum terdaftar'], 404);
+        }
+
+        return response()->json([
+            'message' => 'Data Buku',
+            'data' => $buku,
+        ], 200);
+    }
+}
